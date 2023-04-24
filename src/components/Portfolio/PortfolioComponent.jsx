7d8d3915b85fb9df1e9d7f259aad561257342e72
Jahagiresh <jahangiresh@code.edu.az>
2023-04-24 02:36:25 -0700

    -

---
+++ src/components/Portfolio/PortfolioComponent.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const PortfolioComponent = () => {
+  return (
+    <div className="mt-16 pb-16">
+      <div className="container">
+        <div className="grid max-sm:grid-cols-3 max-lg:grid-cols-2 grid-cols-3">
+          <h1 className="capitalize text-xl font-bold mb-2 sm:col-span-3">
+            xidmətlərimiz
+          </h1>
+
+          <span className="max-sm:hidden">
+            Lorem ipsum dolor sit amet consectetur. Adipiscing tempus morbi
+            rhoncus
+          </span>
+          <Link className="col-span-2 flex justify-end">Hamısını gör</Link>
+        </div>
+        <div className="flex">
+          <div className="box__milky mt-5 ">salam</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PortfolioComponent;
